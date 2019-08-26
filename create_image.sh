595c80c3973b51482167457d47ebb62e48962024
stutommi <tommi.teetee@hotmail.com>
2019-08-26 11:22:35 +0300

    add logic to create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -6,4 +6,7 @@ REPO_NAME=$(echo $URL | cut -d '/' -f 5 | rev | cut -d '.' -f 2 | rev)
 echo "url is $URL"
 echo "repos name is $REPO_NAME"
 git clone $URL test && cd test
+echo "files in dir: $(ls -a)"
+docker build -t stutommi/$REPO_NAME .
+docker push stutommi/$REPO_NAME
 # git clone $URL repo
